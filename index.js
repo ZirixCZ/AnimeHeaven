c643600b19b1563b62b01f7298212833321b0882
ZirixCZ <zirixcz@gmail.com>
2019-08-16 23:07:47 +0200

    hello

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
-function polívka (this){
-    this.style.fontSize = "90px";
- }
+function inviteButton1() {
+    cursor:pointer;
+    window.open("https://animeheaven.es/detail/tokyo-ghoul-sub.79424")
+}
+
+function inviteButton2() {
+    window.open("https://animeheaven.es/detail/tokyo-ghoul-root-a-sub.31400")
+}
+
+function inviteButton3() {
+    window.open("https://animeheaven.es/detail/tokyo-ghoul-re-sub.52747")
+}
+
+function inviteButton4() {
+    window.open("https://animeheaven.es/detail/tokyo-ghoul-re-2nd-season-sub.76620")
+}
 
 
 
